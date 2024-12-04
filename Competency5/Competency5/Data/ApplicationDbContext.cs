c558dfa17b78cc94249c5a7aa194128c99189f94
LUKE ZIOMEK <lziomek@cnm.edu>
2024-12-04 00:52:54 -0700

    Add project files.

---
+++ Competency5/Competency5/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Competency5.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Asset> Assets { get; set; }
+    }
+}
